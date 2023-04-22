259f42b6e6d9245b9dac615b4a41ac0aba304e21
Pål Remme <paal.remme@kulturit.no>
2023-04-22 22:06:22 +0200

    buttons working

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,34 @@ import SoundButton from './components/soundButton/SoundButton'
 
 const Container = styled.div`
   display: flex;
+  gap: 20px;
+  width: 100vw;
+  overflow-y: hidden;
+  flex-wrap: wrap; 
+  place-items: flex-start; 
+  margin: 30px;
 `
 
-function App() {
-  
+function App() {  
 
   return (
     <>
       <Container>
-        <SoundButton/>
+        <SoundButton emoji={"👍"} labelText={"JA"} voiceText={"Ja"} buttonColor={"green"}/>
+        <SoundButton emoji={"👎"} labelText={"NEI"} voiceText={"Nei"} buttonColor={"red"}/>
+        <SoundButton emoji={"🚽"} labelText={"Må på do"} voiceText={"Eg må på do"}/>
+        <SoundButton emoji={"🥛"} labelText={"Tørst"} voiceText={"Eg e tørst"}/>
+        <SoundButton emoji={"🍲"} labelText={"Sulten"} voiceText={"Eg e sulten"}/>
+        <SoundButton emoji={"🚶‍♀️"} labelText={"Gå tur"} voiceText={"Eg vil ut å gå"}/>
+        <SoundButton emoji={"🙂"} labelText={"Bra"} voiceText={"Det går  bra"}/>
+        <SoundButton emoji={"😱"} labelText={"Redd"} voiceText={"Eg e redd"}/>
+        <SoundButton emoji={"😡"} labelText={"Sint"} voiceText={"Eg e sint"}/>       
+        <SoundButton emoji={"😢"} labelText={"Trist"} voiceText={"Eg e lei mæ"}/>        
+        <SoundButton emoji={"🤕"} labelText={"Vondt"} voiceText={"Det gjør vondt"}/>
+        <SoundButton emoji={"🤢"} labelText={"Kvalm"} voiceText={"Eg e kvalm"}/>
+        <SoundButton emoji={"😩"} labelText={"Sliten"} voiceText={"Eg e sliten"}/>
+        <SoundButton emoji={"😴"} labelText={"Sove"} voiceText={"Eg vil sove"}/>
+        <SoundButton emoji={"❤️"} labelText={"Gla i deg"} voiceText={"Gla i dæg!"}/> 
       </Container>
         
     </>
--- src/components/soundButton/SoundButton.jsx
+++ src/components/soundButton/SoundButton.jsx
@@ -3,15 +3,34 @@ import styled from '@emotion/styled'
 const Container = styled.button`
     width: 150px;
     height: 150px;
-    background-color: yellow;
+    background-color: ${props => props.buttonColor};
+    
 `
 
+const Emoji = styled.div`
+    font-size: 2rem;    
+`
+const Label = styled.div`
+    font-size: 1rem;
+` 
 
 
-const SoundButton =()=>{
+
+const SoundButton =({emoji, labelText, voiceText, buttonColor})=>{
+    const voices = speechSynthesis.getVoices();
+    const speech = new SpeechSynthesisUtterance();
+    speech.lang = "NO-nb"
+    speech.pitch = 2
+    speech.rate = 1
+    speech.text = voiceText;    
+    
+    
+    
+    console.log(voices);
     return (
-        <Container>
-                test
+        <Container buttonColor={buttonColor} onClick={()=>{speechSynthesis.speak(speech)}}>
+            <Emoji>{emoji}</Emoji>
+                <Label>{labelText}</Label>
 
         </Container>
     )
