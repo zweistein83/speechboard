f15ce2360bb39a1ba97befc28b1d7ff72533c4df
Pål Remme <paal.remme@kulturit.no>
2023-04-22 20:53:02 +0200

    button

---
+++ src/components/soundButton/SoundButton.jsx
@@ -0,0 +1,20 @@
+import styled from '@emotion/styled'
+
+const Container = styled.button`
+    width: 150px;
+    height: 150px;
+    background-color: yellow;
+`
+
+
+
+const SoundButton =()=>{
+    return (
+        <Container>
+                test
+
+        </Container>
+    )
+
+}
+export default SoundButton
