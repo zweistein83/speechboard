518ff26928a01c611ac3df99bda13e3275cb7e4f
Pål Remme <paal.remme@kulturit.no>
2023-04-23 21:44:19 +0200

    img

--- vite.config.js
+++ vite.config.js
@@ -3,42 +3,6 @@ import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
 
 
-const manifestData = {
- 
-  manifest:{
-    "short_name": "Speechboard",
-    "name": "Speechboard",
-    "icons": [
-      {
-        "src": "/public/images/speechboard_icon_1000.svg",
-        "type": "image/svg+xml",
-        "sizes": "1000x1000"
-      },
-      {
-        "src": "/public/images/speechboard_icon_192.png",
-        "type": "image/png",
-        "sizes": "192x192"
-      },
-      {
-        "src": "/public/images/speechboard_icon_512.png",
-        "type": "image/png",
-        "sizes": "512x512"
-      }
-    ],
-    "dir": "ltr",
-    "start_url": "/speechboard/",
-    "background_color": "#3367D6",
-    "display": "standalone",
-    "scope": "/speechboard/",
-    "theme_color": "#3367D6",    
-    "description": "Buttons that utters words when clicking on them"  ,
-    "display_override": [
-      "standalone",
-      "fullscreen",
-      "minimal-ui"
-    ] 
-  }
-}
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(),    
@@ -49,17 +13,17 @@ export default defineConfig({
         "name": "Speechboard",
         "icons": [
           {
-            "src": "/public/images/speechboard_icon_1000.svg",
+            "src": "/images/speechboard_icon_1000.svg",
             "type": "image/svg+xml",
             "sizes": "1000x1000"
           },
           {
-            "src": "/public/images/speechboard_icon_192.png",
+            "src": "/images/speechboard_icon_192.png",
             "type": "image/png",
             "sizes": "192x192"
           },
           {
-            "src": "/public/images/speechboard_icon_512.png",
+            "src": "/images/speechboard_icon_512.png",
             "type": "image/png",
             "sizes": "512x512"
           }
